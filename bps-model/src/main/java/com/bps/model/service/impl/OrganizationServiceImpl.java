1eab0075a6ce1951a90b9796b0fd6c486d70e676
Shobhit Tyagi <nitw.shobhit@gmail.com>
2015-06-26 19:04:49 -0500

    New

--- bps-model/src/main/java/com/bps/model/service/impl/OrganizationServiceImpl.java
+++ bps-model/src/main/java/com/bps/model/service/impl/OrganizationServiceImpl.java
@@ -54,7 +54,6 @@ public class OrganizationServiceImpl implements OrganizationService {
 		dbService.executeMultipleNamedQueries(setQueries);
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public void editOrganization(OrganizationBean orgBean) {
 		DBFacade<BpsOrganization, Long> dbService = DaoServiceFactory.generateService(ServiceType.Organization);
